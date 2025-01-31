9f4fac58a85e407f50c1dd3da4ceeb242e81e163
Jayden Stuver <jaydenstuver@gmail.com>
2025-01-31 16:56:22 -0700

    get sketchybar working

---
+++ .config/sketchybar/install_sketchybar.sh
@@ -0,0 +1,3 @@
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttfi
+# SbarLua
+(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
